c22ee6eaa63a80235b48fe3be1a3ab60f5ea0166
Maxime Maheo <maxime@luni.app>
2022-02-19 21:46:12 +0100

    Adding Swiftlint

--- Motivation/Controllers/ViewController.swift
+++ Motivation/Controllers/ViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
 }
-
